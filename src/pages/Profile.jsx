6225e41776b2b233050a4a2cff2de996dbfb4bf2
Tobias-soderberg <145038029+Tobias-soderberg@users.noreply.github.com>
2024-08-20 16:58:02 +0200

    Add profile page

---
+++ src/pages/Profile.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+function Profile() {
+  return (
+    <>
+      <div className="profile-page">
+        <div></div>
+      </div>
+    </>
+  );
+}
+
+export default Profile;
