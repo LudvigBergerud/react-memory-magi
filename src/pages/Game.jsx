184260f57c83c9852135389570d045e7af1f1c4d
Tim Nordin <74505667+Sidus-96@users.noreply.github.com>
2024-08-21 17:40:05 +0200

    Ordnat timer börjat med att kolla useritems (klara kort)

--- src/pages/Game.jsx
+++ src/pages/Game.jsx
@@ -1,18 +1,21 @@
-import React, {useState,useEffect} from 'react'
+import React, {useState,useEffect, useRef} from 'react'
 import cardback from '../assets/cardBack.png'
 import cardFront from '../assets/cardFront.png'
 import './Game.css'
 
 
-
-
 function Game() {
   const [items, setItems] = useState([]);
+  const [UserItems, setUserItems] = useState([]);
   const [flippedcards, setNewflipState] = useState({});
   const [listOfFlippedCards, setList] = useState([]);
+  const [time, setTime] = useState(0);
+
 
   let categoryId=3; //Denna behöver routas in beroende på vilket spel användare väljer
   let difficulty ='Easy'; // Samma som ovan, routas in från quiz home page sidan, tillfällig variabel.
+  let userId ="new_user_id"; //samma som ovan, tillfällig variabel
+
 
 const DifficultyEnum =
 {
@@ -20,7 +23,7 @@ const DifficultyEnum =
   Medium:3,
   Hard:4
 }
-console.log("svårighet " + DifficultyEnum[difficulty]);
+
   const handleFlip = (id,index) => {
 console.log("kortid-:"+id)
 
@@ -45,7 +48,18 @@ console.log("kortid-:"+id)
     
 
   };
-  
+   
+
+    useEffect(() => {
+      fetch(`https://localhost:7259/api/UserItem?userId=${userId}`)
+        .then((res) => res.json())
+        .then(data =>{
+          console.log("useritems:"+data);
+          setUserItems(data);
+            }) 
+    }, []);
+
+
     //Hämta korten från en specifik kategori
     useEffect(() => {
       fetch("https://localhost:7259/api/Item")
@@ -67,6 +81,16 @@ console.log("kortid-:"+id)
           })
         
     }, []);
+
+    useEffect(() => {
+      let interval;
+     
+          interval = setInterval(() => {
+              setTime(prevTime => prevTime + 10);
+          }, 10);
+     
+      return () => clearInterval(interval);
+  });
 
     //Denna är aktiv hela tiden och kollar hur många kort som är klickade just nu.
     useEffect(() => {
@@ -95,8 +119,11 @@ console.log("andra"+secondCard);
 
   return (
     <div className="container">
-      <div className='justify-content center'>
-        <h1>klocka</h1>
+      <div className='text-center '>
+      <h1>{("0" + Math.floor((time / 60000) % 60)).slice(-2)}:
+        {("0" + Math.floor((time / 1000) % 60)).slice(-2)}:
+        {("0" + ((time / 10) % 100)).slice(-2)}</h1>
+       
       </div>
   <div className="row" >
 
