9ef5bff32525baeb0add22972f2db731c4fbd100
andrejakobsson7 <andre.jakobsson7@hotmail.com>
2024-08-22 11:17:10 +0200

    Set up initial footer layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import "./App.css";
-import 'bootstrap/dist/css/bootstrap.min.css'; 
+import "bootstrap/dist/css/bootstrap.min.css";
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import Home from "./pages/Home";
 import NotFound from "./pages/NotFound";
@@ -9,6 +9,7 @@ import LandingPage from "./pages/LandingPage";
 import Game from "./pages/Game";
 import Result from "./pages/Result";
 import Profile from "./pages/Profile";
+import Footer from "./components/Footer";
 
 function App() {
   return (
@@ -24,7 +25,7 @@ function App() {
           <Route path="*" element={<NotFound />} />
           <Route path="/landingpage" element={<LandingPage />} />
         </Routes>
-        <footer></footer>
+        <Footer></Footer>
       </Router>
     </AuthProvider>
   );
