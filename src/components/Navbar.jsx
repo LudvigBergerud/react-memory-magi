1b10fa557afb6d2bad47403afb66895efb62130f
andrejakobsson7 <andre.jakobsson7@hotmail.com>
2024-08-30 13:40:34 +0200

    make navbar and footer stay in place without overflowing content

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,7 @@
 import React, { useContext, useEffect, useState } from "react";
 import "../styles/Navbar.css";
 import { NavLink, useNavigate } from "react-router-dom";
+import { Nav } from "react-bootstrap";
 
 import useFetch from "../hooks/useFetch";
 
@@ -9,9 +10,11 @@ import {
   PlusLg,
   BoxArrowRight,
   PersonCircle,
+  List,
 } from "react-bootstrap-icons";
 
 import { AuthContext } from "../contexts/AuthProvider";
+import { NavDropdown } from "react-bootstrap";
 
 function Navbar() {
   const authHandler = useContext(AuthContext);
@@ -34,31 +37,33 @@ function Navbar() {
   return (
     <>
       {/* TODO: IMPLEMENT ERROR HANDLING. */}
-      {authHandler.isAuthenticated ? (
-        <div id="nav-container">
-          <div id="brand-wrapper">
-            <NavLink to="Home">
-              {" "}
-              <House className="icon" /> <span>Hem</span>
-            </NavLink>
-          </div>
-          <div id="navlinks-wrapper">
-            <NavLink to="#">
-              <PlusLg className="icon" />
-              <span>Skapa spel</span>
-            </NavLink>
-            <NavLink to="/profile">
-              <PersonCircle />
-              <span>Profil</span>
-            </NavLink>
-            <a onClick={handleLogOut}>
-              <BoxArrowRight className="icon" /> <span>Logga ut</span>
-            </a>
-          </div>
+      <div id="nav-container" className="fixed-top">
+        <div id="brand-wrapper">
+          <NavLink to="Home">
+            {" "}
+            <House className="icon" /> <span>Hem</span>
+          </NavLink>
         </div>
-      ) : (
-        ""
-      )}
+        {authHandler.isAuthenticated ? (
+          <>
+            <div id="navlinks-wrapper">
+              <NavLink to="#">
+                <PlusLg className="icon" />
+                <span>Skapa spel</span>
+              </NavLink>
+              <NavLink to="/profile">
+                <PersonCircle />
+                <span>Profil</span>
+              </NavLink>
+              <a onClick={handleLogOut}>
+                <BoxArrowRight className="icon" /> <span>Logga ut</span>
+              </a>
+            </div>
+          </>
+        ) : (
+          ""
+        )}
+      </div>
     </>
   );
 }
